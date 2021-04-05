26a719ff6dae4c4236b9cd19c7863e37677e5f8f
Oleg <odasheyev@mail.ru>
2021-04-06 00:13:57 +0800

    Added Manager

---
+++ src/main/java/ru/netology/postmanager/PostManager.java
@@ -0,0 +1,23 @@
+package ru.netology.postmanager;
+
+import ru.netilogy.domain.Post;
+
+public class PostManager {
+
+    private Post[] posts;
+
+    public Post[] getPostsForFeed() {
+        // TODO: add logic
+        return null;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset,
+                         int extended, int fields) {
+        // TODO: add logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+        // TODO: add logic
+    }
+}
